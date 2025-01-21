29da371617a059e5d5a6d7b56b1caebf9f2c9e01
thomasandrewlee <thomaslee@princeton.edu>
2025-01-21 09:53:37 -0500

    addedmicroseismclimatology

---
+++ MicroseismClimatology.jl
@@ -0,0 +1,26 @@
+# MicroseismClimatology.jl
+#=
+This code will read in spectral data from a given station (e.g., HRV)
+and will calculate the climatology (i.e., annual average) spectra for
+the time step and window desired (e.g., daily steps, 7-day average).
+
+These spectra can then be used to compute the transfer functions to 
+correct for noise differences at different times of the year, for
+example, in a code like the EQ doublet calculation.
+
+Thomas Lee
+January 13, 2025
+
+=#
+
+## PACKAGES
+
+## SETTINGS
+
+## DATA READ
+# same as in MicroseismActivityIndex
+
+## CLIMATOLOGY CALCULATION
+
+## SAVE AS SPECTRAS
+ 
