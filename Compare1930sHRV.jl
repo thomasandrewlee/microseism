d3887ec6a5d5910a61e6ad3cbbb1f729a1e86fd0
thomasandrewlee <thomaslee@princeton.edu>
2025-02-19 13:22:58 -0500

    dfghjk

--- Compare1930sHRV.jl
+++ Compare1930sHRV.jl
@@ -56,6 +56,9 @@ c_dataout = string(usr_str,"Desktop/1930sComp/TEST5.5_microcorr_wideband_standar
 c_savespect_new = string(usr_str,"Desktop/MAI/HRV_BHZ_1988_2023_spectsave_100prct_1hr_NEW_SACPZ.jld")
 #c_savespect_new = string(usr_str,"Desktop/MAI/HRV_BHZ_1988_2023_spectsave_100prct_1hr_NEW_MICROMETRICMOD.jld")
 c_savespect_old = string(usr_str,"Desktop/MAI/HRV_BHZ_1936_1940_spectsave_100prct_1hr_NEW.jld")
+c_savespect_new = string(usr_str,"Desktop/MAI/HRV_BHZ_1988_2023_spectsave_50prct_12hr_STANDARD.jld")
+#c_savespect_new = string(usr_str,"Desktop/MAI/HRV_BHZ_1988_2023_spectsave_50prct_12hr_MICROMETRICMOD.jld")
+c_savespect_old = string(usr_str,"Desktop/MAI/HRV_BHZ_1935_1940_spectsave_50prct_12hr_STANDARD.jld")
 readin_new = "standard" # regular way from MicroseismActivityIndex.jl
 #readin_new = "rickmicrometric" # convert to velocity and divide into bands
 # plotting
