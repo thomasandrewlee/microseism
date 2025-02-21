d8a2159751cccd5634cff2fbd7d74520d6fd1194
thomasandrewlee <thomaslee@princeton.edu>
2025-02-21 12:39:52 -0500

    fghjk

--- primarycoefficients.m
+++ primarycoefficients.m
@@ -17,6 +17,7 @@ user_str = '/Users/tl7869/';
 c_bathy = [user_str,'Research/GEBCO_Bathymetry/' ...
     'gebco_2024/GEBCO_2024.nc'];
 bathy_deg_size = 0.5; % size of bathymetry grid boxes in degrees, will interpolate if need 
+c_ww3 = [user_str,'Downloads/WW3_OUT_NEW_EF/'];
 c_output = [user_str,'Downloads/PrimaryCoef0.5/'];
 c_lindisp = [user_str,'Desktop/MicroseismIntegration/lindisptables/'];
 freqs = 1 ./ [1:0.5:40];
@@ -30,100 +31,105 @@ if ~isfolder(c_output)
     mkdir(c_output);
 end
 
-%% read bathymetry
-disp('Info for bathy file is:')
-ncdisp(c_bathy)
-% get varnames and add to Ctmp struct
-Btmp = ncinfo(c_bathy);
-% get data
-blat = ncread(c_bathy,'lat');
-blon = ncread(c_bathy,'lon'); 
-bathy = ncread(c_bathy,'elevation');
-% decimate bathymetry if appropriate
-if 360/bathy_deg_size < length(blon)
-    disp('Scaling GEBCO down...');
-    blat0 = blat;
-    blon0 = blon;
-    bathy0 = bathy;
-    % new versions
-    blat = min(blat0):bathy_deg_size:max(blat0);
-    blon = min(blon0):bathy_deg_size:max(blon0);
-    blon = reshape(blon,length(blon),1);
-    bathy = interp2(blat0,blon0,double(bathy0),blat,blon);
-    % remove the old high-fidelity versions
-    clear blat0 blon0 bathy0
+% check for save file
+if isfile([c_output,'Bweight.mat'])
+    load([c_output,'Bweight.mat']);
 else
-    warning('Requested bathy grid is finer than GEBCO, using GEBCO...')
-end
-% bathy plot
-hf = figure; ha = axes;
-imagesc(blon,blat,bathy');
-cmtmp = landSeaColormap(1000);
-colormap(cmtmp);
-cb = colorbar; cb.Label.String = 'Elevation (m)';
-ha.YDir = 'normal';
-ha.Title.String = 'GEBCO Bathymetry';
-axis image;
-savefig([c_output,'bathy'],hf.Number,'pdf');
-close(hf);
-
-%% compute wavenumbers
-% convert bathy to h linspaced
-h = linspace(0,double(-min(bathy,[],"all")),Nh);
-% linspace wavenumbers
-%k = linspace(kmin,kmax,Nk);
-% check for existing table
-c_table = ['h_',num2str(h(1)),'_',num2str(h(end)),'_',num2str(length(h)),...
-    '_k_',num2str(k(1)),'_',num2str(k(end)),'_',num2str(length(k)),'.mat'];
-if isfile([c_lindisp,c_table])
-    % load table
-    load([c_lindisp,c_table])
-else
-    % make new table
-    [htab,ktab] = meshgrid(h,k);
-    omgatab = sqrt(9.81*ktab.*tanh(ktab.*htab)); % radian wave freq
-    ftab = omgatab / (2*pi); % convert to freq
-    save([c_lindisp,c_table],'htab','ktab','ftab');
-end
-% compute table lookup on grid for primary in neither shallow or deep water
-kdat = nan([size(bathy) length(freqs)]);
-lonlen = length(blon); % separate to avoid repeat length calls
-% get k values 
-kq = unique(ktab);
-% loop it
-parfor i = 1:length(blat)
-%for i = 1:length(Wlat)
-    disp(['i=',num2str(i)])
-    for j = 1:lonlen
-        % disp(['i=',num2str(i),', j=',num2str(j)])
-        % compute the wavenumber as a function of freq curve for depth h
-        if ~isnan(bathy(j,i)) % skip if no bathymetry data
-            if bathy(j,i)<0 % over water (negative height)
-                % convert height to depth
-                hq = -ones(length(kq),1).*bathy(j,i); 
-                % get frequencies for all k at specific h
-                fq = interp2(htab,ktab,ftab,hq,kq);
-                %do 1D interp of existing freq wavenumber profile
-                ktmp = interp1(fq,kq,freqs,'pchip','extrap');
-                % save value
-                kdat(j,i,:) = ktmp;
+    %% read bathymetry
+    disp('Info for bathy file is:')
+    ncdisp(c_bathy)
+    % get varnames and add to Ctmp struct
+    Btmp = ncinfo(c_bathy);
+    % get data
+    blat = ncread(c_bathy,'lat');
+    blon = ncread(c_bathy,'lon'); 
+    bathy = ncread(c_bathy,'elevation');
+    % decimate bathymetry if appropriate
+    if 360/bathy_deg_size < length(blon)
+        disp('Scaling GEBCO down...');
+        blat0 = blat;
+        blon0 = blon;
+        bathy0 = bathy;
+        % new versions
+        blat = min(blat0):bathy_deg_size:max(blat0);
+        blon = min(blon0):bathy_deg_size:max(blon0);
+        blon = reshape(blon,length(blon),1);
+        bathy = interp2(blat0,blon0,double(bathy0),blat,blon);
+        % remove the old high-fidelity versions
+        clear blat0 blon0 bathy0
+    else
+        warning('Requested bathy grid is finer than GEBCO, using GEBCO...')
+    end
+    % bathy plot
+    hf = figure; ha = axes;
+    imagesc(blon,blat,bathy');
+    cmtmp = landSeaColormap(1000);
+    colormap(cmtmp);
+    cb = colorbar; cb.Label.String = 'Elevation (m)';
+    ha.YDir = 'normal';
+    ha.Title.String = 'GEBCO Bathymetry';
+    axis image;
+    savefig([c_output,'bathy'],hf.Number,'pdf');
+    close(hf);
+    
+    %% compute wavenumbers
+    % convert bathy to h linspaced
+    h = linspace(0,double(-min(bathy,[],"all")),Nh);
+    % linspace wavenumbers
+    %k = linspace(kmin,kmax,Nk);
+    % check for existing table
+    c_table = ['h_',num2str(h(1)),'_',num2str(h(end)),'_',num2str(length(h)),...
+        '_k_',num2str(k(1)),'_',num2str(k(end)),'_',num2str(length(k)),'.mat'];
+    if isfile([c_lindisp,c_table])
+        % load table
+        load([c_lindisp,c_table])
+    else
+        % make new table
+        [htab,ktab] = meshgrid(h,k);
+        omgatab = sqrt(9.81*ktab.*tanh(ktab.*htab)); % radian wave freq
+        ftab = omgatab / (2*pi); % convert to freq
+        save([c_lindisp,c_table],'htab','ktab','ftab');
+    end
+    % compute table lookup on grid for primary in neither shallow or deep water
+    kdat = nan([size(bathy) length(freqs)]);
+    lonlen = length(blon); % separate to avoid repeat length calls
+    % get k values 
+    kq = unique(ktab);
+    % loop it
+    parfor i = 1:length(blat)
+    %for i = 1:length(Wlat)
+        disp(['i=',num2str(i)])
+        for j = 1:lonlen
+            % disp(['i=',num2str(i),', j=',num2str(j)])
+            % compute the wavenumber as a function of freq curve for depth h
+            if ~isnan(bathy(j,i)) % skip if no bathymetry data
+                if bathy(j,i)<0 % over water (negative height)
+                    % convert height to depth
+                    hq = -ones(length(kq),1).*bathy(j,i); 
+                    % get frequencies for all k at specific h
+                    fq = interp2(htab,ktab,ftab,hq,kq);
+                    %do 1D interp of existing freq wavenumber profile
+                    ktmp = interp1(fq,kq,freqs,'pchip','extrap');
+                    % save value
+                    kdat(j,i,:) = ktmp;
+                end
             end
         end
     end
-end
-
-%% compute bathymetric effect
-Bweight = nan(size(kdat));
-for i = 1:length(blon)
-    for j = 1:length(blat)
-        Bweight(i,j,:) = 1./cosh(kdat(i,j,:).*-bathy(i,j));
-        % bathymetry weighting from: Effective Water Depth Correction 
-        % for Pressure-Based Wave Statistics on Rough Bathymetry 
-        % (Maruqes, Fedderson, McMahan)
+    
+    %% compute bathymetric effect
+    Bweight = nan(size(kdat));
+    for i = 1:length(blon)
+        for j = 1:length(blat)
+            Bweight(i,j,:) = 1./cosh(kdat(i,j,:).*-bathy(i,j));
+            % bathymetry weighting from: Effective Water Depth Correction 
+            % for Pressure-Based Wave Statistics on Rough Bathymetry 
+            % (Maruqes, Fedderson, McMahan)
+        end
     end
+    % save data
+    save([c_output,'Bweight.mat'],'Bweight','blon','blat','freqs','-mat','-v7.3');
 end
-% save data
-save([c_output,'Bweight.mat'],'Bweight','blon','blat','freqs','-mat','-v7.3');
 
 %% plot
 %clow = min(log10(Bweight),[],"all","omitnan");
